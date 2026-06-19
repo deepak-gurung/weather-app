048bbcb01fb2a5c704e693845854dab3c0982fe7
deepakgurung <deepakgurung459@gmail.com>
2026-06-19 23:17:00 +0545

    setup api key and get data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import TopButton from './components/TopButton'
-import InputField from './components/InputField'
-import TimeAndLocation from './components/TimeAndLocation'
-import TemperatureAndDetails from './components/TemperatureAndDetails'
-import ForeCast from './components/ForeCast'
+import React from "react";
+import TopButton from "./components/TopButton";
+import InputField from "./components/InputField";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import ForeCast from "./components/ForeCast";
+import getFormattedWeatherData from "./services/weatherServices";
 
 const App = () => {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
-    <div className='h-screen bg-gradient-to-br from-cyan-700 to-blue-700'>
+    <div className="h-screen bg-gradient-to-br from-cyan-700 to-blue-700">
       <TopButton />
       <InputField />
       <TimeAndLocation />
       <TemperatureAndDetails />
       <ForeCast title="HOURLY FORECAST" />
       <ForeCast title="WEEKLY FORECAST" />
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/services/weatherServices.js
@@ -0,0 +1,51 @@
+const API_KEY = "3d8610f69a66c831448c19656b54a4a3";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams,
+  ).then(formatCurrentWeather);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
