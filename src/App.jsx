fff019a0d4f5593abf8838059b66a58c518ce11e
deepakgurung <deepakgurung459@gmail.com>
2026-06-20 08:17:25 +0545

    updated the final code

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import InputField from "./components/InputField";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import ForeCast from "./components/ForeCast";
-import getFormattedWeatherData from "./services/weatherServices";
 
 const App = () => {
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data);
-  };
-
-  fetchWeather();
-
   return (
     <div className="h-screen bg-gradient-to-br from-cyan-700 to-blue-700">
       <TopButton />
